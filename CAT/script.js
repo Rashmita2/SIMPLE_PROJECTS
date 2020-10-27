1cbcd6b5049b101e602ed449b137bfe868086047
Rashmita Khatri <67445848+Rashmita2@users.noreply.github.com>
2020-10-26 19:07:52 -0500

    Add files via upload

---
+++ CAT/script.js
@@ -0,0 +1,7 @@
+
+function generatedog(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-dog');
+    image.src = "https://media0.giphy.com/media/SX71qs3TDxVkvbLJ4o/giphy.gif?cid=ecf05e4785374e15cff54cc0ea4ecf0ef093cd71ed82b486&rid=giphy.gif";
+    div.appendChild(image);
+}
